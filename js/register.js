5f99bc03cb026a6d1b805a8038326534bf6e127f
itsenree <75910282+itsenree@users.noreply.github.com>
2026-04-29 14:12:56 +0200

    register js

--- js/register.js
+++ js/register.js
@@ -1,1 +1,8 @@
 console.log("Register.js ist verbunden!");
+
+document.getElementById("registerForm")
+.addEventListener("submit", async (e) => {
+    // hier wird hingeschrieben was beim submit passiert
+    e.preventDefault();
+console.log("Submit");
+});
