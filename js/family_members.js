8445f157e4f3c395fbcddcee10c1087304d1119f
itsenree <75910282+itsenree@users.noreply.github.com>
2026-05-13 10:24:39 +0200

    members page angefangen

---
+++ js/family_members.js
@@ -0,0 +1,27 @@
+const addBtn = document.getElementById("addBtn");
+const memberForm = document.getElementById("memberForm");
+
+addBtn.addEventListener("click", () => {
+  memberForm.classList.toggle("hidden");
+});
+
+document.getElementById("confirmBtn").addEventListener("click", async () => {
+
+  const data = {
+    name: document.getElementById("name").value,
+    color: document.getElementById("color").value,
+    brush_nr: document.getElementById("brushNr").value
+  };
+
+  const response = await fetch("/add-member", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  if (response.ok) {
+    alert("Member added!");
+  }
+});
